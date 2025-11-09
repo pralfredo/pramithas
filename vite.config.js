6f9306afb7aa7e2fa3499f22de6b6ac1ea726fd0
pralfredo <132622261+pralfredo@users.noreply.github.com>
2025-11-09 16:15:41 -0500

    Optimization

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  base: '/pramithas/',
-  publicDir: 'public', // needed so vite copies your /draco/ folder
+  base: '/',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     target: 'esnext',
